3c5b94749fb4584589d8e151b8f7f330d2322908
Alexandra Chakeres <alexchakeres@gmail.com>
2019-06-24 15:18:54 -0600

    Add ItemsController#update (& render JSON in update & create actions)

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -10,6 +10,13 @@ class Api::V1::ItemsController < ApplicationController
   def create
     item = Item.new(item_params)
     item.save
+    render json: item
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    render json: item
   end
 
   private
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -32,5 +32,24 @@ describe "Items API" do
 
     expect(response).to be_successful
     expect(item.name).to eq(item_params[:name])
+
+    item_from_JSON = JSON.parse(response.body)
+    expect(item_from_JSON["name"]).to eq(item_params[:name])
+  end
+
+  it "can update an existing item" do
+    id = create(:item).id
+    previous_name = Item.last.name
+    item_params = { name: "Sledge" }
+
+    put "/api/v1/items/#{id}", params: {item: item_params}
+    item = Item.find_by(id: id)
+
+    expect(response).to be_successful
+    expect(item.name).to_not eq(previous_name)
+    expect(item.name).to eq(item_params[:name])
+
+    item_from_JSON = JSON.parse(response.body)
+    expect(item_from_JSON["name"]).to eq(item_params[:name])
   end
 end
